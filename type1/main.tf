3640dd03492073d00bf01fb4c7a1759d56cb53ea
Aravind Sudhakaran <124213290+Arvisudz@users.noreply.github.com>
2025-11-22 16:31:10 +0100

    Update main.tf

--- type1/main.tf
+++ type1/main.tf
@@ -5,10 +5,28 @@ provider "aws" {
 resource "aws_instance" "example_instance" {
   ami           = "ami-02b8269d5e85954ef" 
   instance_type = "t3.micro" 
-  subnet_id = "subnet-0337081fbe4ae5ee6" 
+  subnet_id     = "subnet-0337081fbe4ae5ee6" 
   vpc_security_group_ids = ["sg-0d0e4740d9e17386d"]
   
   tags = {
     Name = "EC2Instance_2"
+  }
+}
+
+resource "aws_s3_bucket" "example_bucket" {
+  bucket = "aravind-demo-bucket-12345"
+
+  tags = {
+    Name = "DemoS3Bucket"
+  }
+}
+
+resource "aws_ecr_repository" "example_ecr" {
+  name = "aravind-demo-ecr"
+
+  image_tag_mutability = "MUTABLE"
+
+  tags = {
+    Name = "DemoECRRepository"
   }
 }
