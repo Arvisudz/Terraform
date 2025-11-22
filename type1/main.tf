3480afff54c47fb013af85b7a24daa303f1c5ce2
Aravind Sudhakaran <124213290+Arvisudz@users.noreply.github.com>
2025-11-22 16:13:11 +0100

    Create main.tf

---
+++ type1/main.tf
@@ -0,0 +1,14 @@
+provider "aws" {
+  region = "ap-south-1"
+}
+
+resource "aws_instance" "example_instance" {
+  ami           = "ami-02b8269d5e85954ef" 
+  instance_type = "t3.micro" 
+  subnet_id = "subnet-0337081fbe4ae5ee6" 
+  vpc_security_group_ids = "sg-0d0e4740d9e17386d"
+  
+  tags = {
+    Name = "EC2Instance_2"
+  }
+}
