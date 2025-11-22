2862029bdbd93b831cd57616452522332873f1fc
Aravind Sudhakaran <124213290+Arvisudz@users.noreply.github.com>
2025-11-22 16:26:57 +0100

    Update main.tf

--- type1/main.tf
+++ type1/main.tf
@@ -6,7 +6,7 @@ resource "aws_instance" "example_instance" {
   ami           = "ami-02b8269d5e85954ef" 
   instance_type = "t3.micro" 
   subnet_id = "subnet-0337081fbe4ae5ee6" 
-  vpc_security_group_ids = "sg-0d0e4740d9e17386d"
+  vpc_security_group_ids = ["sg-0d0e4740d9e17386d"]
   
   tags = {
     Name = "EC2Instance_2"
